11e3b5378823e22f52e1e83936046e01776a31d6
E-ugine <agollaeugine@gmail.com>
2026-09-21 16:28:51 +0300

    Fix: Remove hardcoded dataabase_url in config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql+psycopg://postgres:postgres@localhost:5432/ingestion_engine"
+    database_url: str 
 
     model_config = SettingsConfigDict(env_file=".env", env_prefix="")
 
